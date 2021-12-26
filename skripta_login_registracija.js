08a77b03b3715cf63ecc5f4555815cac85b9f720
ealic4 <ealic4@etf.unsa.ba>
2021-12-26 21:24:53 +0100

    Urađen login i registracija

---
+++ skripta_login_registracija.js
@@ -0,0 +1,13 @@
+function OpenWindowLogin(){
+    document.getElementById("cijelilogin").style.display = "flex";
+}
+function CloseLogin(){
+    document.getElementById("cijelilogin").style.display = "none";
+}
+
+function OpenWindowRegistration(){
+    document.getElementById("cijelaregistracija").style.display = "flex";
+}
+function CloseRegistration(){
+    document.getElementById("cijelaregistracija").style.display = "none";
+}
